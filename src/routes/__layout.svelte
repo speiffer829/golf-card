b506c94bbcbca7072892f14f510ff2fc7b4f413b
Spenser <spenser@minddnd.com>
2021-07-01 11:57:15 -0400

    start button

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -3,22 +3,22 @@
 </script>
 
 <h1>Golf Scorerer</h1>
-<div class="wrap">
+<main class="wrap">
 	<slot />
-</div>
+</main>
 
 <style>
 	h1{
 		font-weight: 900;
 		color: var(--gold);
-		text-shadow: 2px 2px 0 var(--brown), 4px 4px 0 var(--red), 6px 6px var(--green);
+		text-shadow: var(--shroom-text-shadow);
 		/* background: linear-gradient(to top, var(--sea), var(--sea-light)); */
 		font-family: var(--font-title);
 		text-align: center;
 		width: calc(100% - 55px);
 		margin: 55px auto 50px;
 		padding: 10px;
-		border-radius: 5px;
+		border-radius: 10px;
 		box-sizing: border-box;
 		box-shadow: 0px 0px 0px 5px var(--gold), 0px 0px 0px 10px var(--green), 0px 0px 0px 15px var(--red);
 		font-size: clamp(2rem, 9vw, 3.5rem);
@@ -28,5 +28,6 @@
 	.wrap{
 		max-width: 500px;
 		margin: 20px auto;
+		padding: 20px;
 	}
 </style>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -4,3 +4,40 @@
 	<title>Golf Scorerer</title>
 </svelte:head>
 
+<div class="border">
+	<h2>Thing</h2>
+	<p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Iusto beatae porro maxime dolorum itaque aliquam, ullam velit libero adipisci similique earum, facere iure voluptates sapiente quos harum dolor voluptas eaque.</p>
+</div>
+
+<button>Start</button>
+
+<style lang="scss">
+	button{
+		font-size: 2rem;
+		text-align: center;
+		border: none;
+		background: var(--green);
+		color: var(--gold);
+		cursor: pointer;
+		font-weight: bold;
+		font-family: var(--font-title);
+		padding: 20px 40px;
+		margin: 20px auto;
+		display: block;
+		letter-spacing: .2em;
+		border-radius: 10px;
+		box-shadow: 5px 5px var(--gold), 10px 10px var(--red);
+		transition: all 100ms;
+
+		&:active{
+			transform: scale(.9);
+			box-shadow: 2px 2px var(--gold), 4px 4px var(--red);
+		}
+	}
+
+	.border{
+		padding: 20px;
+		border: solid 5px var(--gold);
+		border-radius: 10px;
+	}
+</style>
