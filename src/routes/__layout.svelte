8b94e44c2a9d0903abb2c07172e8d62ab6369f4b
Spenser <spenser@minddnd.com>
2021-07-01 10:48:59 -0400

    header

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,6 +1,32 @@
 <script>
 	import '../app.scss'
 </script>
-<div class="foo">
-	<h1>Hello</h1>
+
+<h1>Golf Scorerer</h1>
+<div class="wrap">
+	<slot />
 </div>
+
+<style>
+	h1{
+		font-weight: 900;
+		color: var(--gold);
+		text-shadow: 2px 2px 0 var(--brown), 4px 4px 0 var(--red), 6px 6px var(--green);
+		/* background: linear-gradient(to top, var(--sea), var(--sea-light)); */
+		font-family: var(--font-title);
+		text-align: center;
+		width: calc(100% - 55px);
+		margin: 55px auto 50px;
+		padding: 10px;
+		border-radius: 5px;
+		box-sizing: border-box;
+		box-shadow: 0px 0px 0px 5px var(--gold), 0px 0px 0px 10px var(--green), 0px 0px 0px 15px var(--red);
+		font-size: clamp(2rem, 9vw, 3.5rem);
+		
+	}
+
+	.wrap{
+		max-width: 500px;
+		margin: 20px auto;
+	}
+</style>
---
+++ src/routes/index.svelte
@@ -0,0 +1,6 @@
+
+
+<svelte:head>
+	<title>Golf Scorerer</title>
+</svelte:head>
+
