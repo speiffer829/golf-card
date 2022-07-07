fedcfc484279e61a3feafa7cb22caac25f543386
Spenser <spenser@minddnd.com>
2022-07-07 13:08:29 -0400

    basic line chart works

--- src/lib/components/ScoreCard.svelte
+++ src/lib/components/ScoreCard.svelte
@@ -1,38 +1,60 @@
 <script>
-	import {slide} from 'svelte/transition'
-	
+	import { slide } from 'svelte/transition';
+	import Chart from 'svelte-frappe-charts';
+	import { currentHole } from '$lib/stores/store';
+
 	export let player;
 	export let i;
 
 	export let showGrid = false;
+
+	$: labels = player.holes.filter((h, index) => h !== 0).map((h, index) => `${index + 1}`);
+
+	$: data = {
+		labels,
+		datasets: [
+			{
+				chartType: 'line',
+				values: player.holes
+			}
+		]
+	};
 </script>
 
 <div class="player-score-box">
 	<div class="main-row">
-			<p class="player-name">{i + 1}. {player.name}</p>
-			<p class="player-total">{ player.holes.reduce((a, b) => a+b) }</p>
-			
+		<p class="player-name">{i + 1}. {player.name}</p>
+		<p class="player-total">{player.holes.reduce((a, b) => a + b)}</p>
 	</div>
 	{#if showGrid}
-		<div class="grid" transition:slide={{duration: 300}}>
+		<div class="grid" transition:slide|local={{ duration: 300 }}>
 			{#each player.holes as hole, i}
-				<div class="hole" class:faded={hole === 0} class:green={hole < 3 && hole !== 0} class:red={hole > 4}>
-					<p class="hole-number" class:green={hole < 3 && hole !== 0} class:red={hole > 4}><sup>#</sup>{ i + 1 }</p>
-					<p class="hole-score" class:green={hole < 3 && hole !== 0} class:red={hole > 4}>{ hole }</p>
+				<div
+					class="hole"
+					class:faded={hole === 0}
+					class:green={hole < 3 && hole !== 0}
+					class:red={hole > 4}
+				>
+					<p class="hole-number" class:green={hole < 3 && hole !== 0} class:red={hole > 4}>
+						<sup>#</sup>{i + 1}
+					</p>
+					<p class="hole-score" class:green={hole < 3 && hole !== 0} class:red={hole > 4}>{hole}</p>
 				</div>
 			{/each}
-		</div>	
+		</div>
+		<div transition:slide|local={{ duration: 300 }}>
+			<Chart {data} height={150} colors={['#689b85']} lineOptions={{ regionFill: 1 }} />
+		</div>
 	{/if}
 </div>
 
 <style lang="scss">
-
-	.player-score-box{
+	.player-score-box {
 		width: 100%;
 		border-bottom: solid 3px var(--brown);
 	}
 
-	.main-row{
+	.main-row {
 		display: flex;
 		padding: 2rem 0rem 1.5rem;
 		text-shadow: 2px 2px var(--red), 4px 4px var(--green);
@@ -41,17 +63,17 @@
 		font-family: var(--font-title);
 		font-weight: bold;
 
-		.player-name{
+		.player-name {
 			flex: 1 1 auto;
 			text-transform: capitalize;
 		}
 
-		.player-total{
+		.player-total {
 			flex: 0 0 auto;
 			padding-left: 1rem;
 		}
 	}
-	.grid{
+	.grid {
 		display: grid;
 		grid-template-columns: repeat(9, 1fr);
 		width: 100%;
@@ -59,27 +81,27 @@
 		padding-bottom: 1rem;
 	}
 
-	.hole{
+	.hole {
 		--holeColor: var(--brown);
 		text-align: center;
 		border: solid 1px var(--holeColor, var(--brown));
 		overflow: hidden;
 		border-radius: 3px;
 
-		&.faded{
+		&.faded {
 			opacity: 0.3;
 		}
 
-			&.red{
-			--holeColor: var(--red)
+		&.red {
+			--holeColor: var(--red);
 		}
 
-		&.green{
+		&.green {
 			--holeColor: var(--green);
 		}
 	}
 
-	.hole-number{
+	.hole-number {
 		background: var(--holeColor, var(--brown));
 		width: 100%;
 		margin: 0px auto 0;
@@ -87,15 +109,13 @@
 		color: var(--pale-yellow);
 	}
 
-	sup{
+	sup {
 		font-size: 0.5rem;
 	}
 
-	.hole-score{
+	.hole-score {
 		font-weight: bold;
 		margin-top: 3px;
 		color: var(--holeColor, var(--brown));
 	}
-
 </style>
-
